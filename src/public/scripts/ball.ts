926c654582a8eccbe45ec525e959fd2d4bc03da1
Mart100 <martvanenck1@gmail.com>
2020-11-05 14:20:33 +0100

    switch raycollision from ball to ray

--- src/public/scripts/ball.ts
+++ src/public/scripts/ball.ts
@@ -3,13 +3,6 @@ import { world } from "./main";
 import { Ray } from "./ray";
 import { Vec3 } from "./vec3";
 
-
-interface rayBallIntersect {
-	hittable:Hittable
-	rayLength:number
-	intersectPos:Vec3
-	ray:Ray
-}
 export class Ball extends Hittable {
 
 	constructor(options?:HittableOptions) {
@@ -18,28 +11,4 @@ export class Ball extends Hittable {
 		super(options)
 	}
 
-
-	getRayIntersects(ray:Ray) : Array<rayBallIntersect> {
-		let intersects = []
-		for(let hittable of world.hittables) {
-			let eye_to_centerBall = hittable.pos.clone().subtract(ray.pos)
-			let rayLength = eye_to_centerBall.dotProduct(ray.vel)
-			let rayClosestToBall = ray.pos.clone().plus(ray.vel.clone().setMagnitude(rayLength))
-			let rayDistanceToBall = rayClosestToBall.clone().subtract(hittable.pos).getMagnitude()
-			if(rayDistanceToBall < hittable.size/2 && rayLength > 0) {
-				let dist1 = rayLength-Math.sqrt((hittable.size/2)**2 - rayDistanceToBall**2)
-				let intersectPos = ray.pos.clone().plus(ray.vel.clone().setMagnitude(dist1))
-				let intersect:rayBallIntersect = {
-					rayLength,
-					hittable,
-					intersectPos,
-					ray
-				}
-				intersects.push(intersect)
-			}
-		}
-		let intersectsOrdered = intersects.sort((a:rayBallIntersect, b:rayBallIntersect) => a.rayLength - b.rayLength)
-		if(!intersectsOrdered[0]) return []
-		return intersects
-	}
 }
--- src/public/scripts/ray.ts
+++ src/public/scripts/ray.ts
@@ -1,6 +1,14 @@
 import { Color } from "./color"
+import { Hittable } from "./hittable"
 import { world } from "./main"
 import { Vec3 } from "./vec3"
+
+export interface rayBallIntersect {
+	hittable:Hittable
+	rayLength:number
+	intersectPos:Vec3
+	ray:Ray
+}
 
 export class Ray {
 
@@ -15,6 +23,33 @@ export class Ray {
 
 		world.raysCreated += 1
 	}
+
+	getRayIntersects() : Array<rayBallIntersect> {
+		let intersects = []
+		let ray = this
+
+		for(let hittable of world.hittables) {
+			let eye_to_centerBall = hittable.pos.clone().subtract(ray.pos)
+			let rayLength = eye_to_centerBall.dotProduct(ray.vel)
+			let rayClosestToBall = ray.pos.clone().plus(ray.vel.clone().setMagnitude(rayLength))
+			let rayDistanceToBall = rayClosestToBall.clone().subtract(hittable.pos).getMagnitude()
+			if(rayDistanceToBall < hittable.size/2 && rayLength > 0) {
+				let dist1 = rayLength-Math.sqrt((hittable.size/2)**2 - rayDistanceToBall**2)
+				let intersectPos = ray.pos.clone().plus(ray.vel.clone().setMagnitude(dist1))
+				let intersect:rayBallIntersect = {
+					rayLength,
+					hittable,
+					intersectPos,
+					ray
+				}
+				intersects.push(intersect)
+			}
+		}
+		let intersectsOrdered = intersects.sort((a:rayBallIntersect, b:rayBallIntersect) => a.rayLength - b.rayLength)
+		if(!intersectsOrdered[0]) return []
+		return intersects
+	}
+
 	getColor() : Color {
 		return new Color().random()
 	}
