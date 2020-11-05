dc3f7084edee095b59201fcc7ad4479e51b0820d
Mart100 <martvanenck1@gmail.com>
2020-11-05 15:04:06 +0100

    test rayCollisionCount 100 times

--- tests/rayCollision.test.ts
+++ tests/rayCollision.test.ts
@@ -9,20 +9,23 @@ import { World } from '../src/public/scripts/world';
 
 
 test('rayBallCollision-count', () => {
-	let world = new World()
-	let ray = new Ray(new Vec3(0,0,0), new Vec3(0, 1, 0), world)
-	let amount = Math.floor(Math.random()*100)+100
-	for(let i=0;i<amount;i++) {
-		let ball = new Ball({
-			pos: new Vec3(0, 100 + i*100, 0),
-			color: new Color(0,0,0),
-			reflection:0,
-			diffusion:0,
-			size:5,
-		})
-		world.addHittable(ball)
+	for(let i=0;i<100;i++) {
+		let world = new World()
+		let ray = new Ray(new Vec3(0,0,0), new Vec3(0, 1, 0), world)
+		let amount = Math.floor(Math.random()*100)+100
+		for(let j=0;j<amount;j++) {
+			let ball = new Ball({
+				pos: new Vec3(0, 100 + j*100, 0),
+				color: new Color(0,0,0),
+				reflection:0,
+				diffusion:0,
+				size:5,
+			})
+			world.addHittable(ball)
+		}
+	
+	
+		expect(ray.getRayIntersects().length).toBe(amount)
 	}
 
-
-	expect(ray.getRayIntersects().length).toBe(amount)
 })
