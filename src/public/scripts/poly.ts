438dee4de9657312e1682b091ff3b24f4daff138
Mart100 <martvanenck1@gmail.com>
2020-11-17 14:55:07 +0100

    start of line-poly intersect detecting

--- src/public/scripts/poly.ts
+++ src/public/scripts/poly.ts
@@ -2,7 +2,7 @@ import { Hittable, HittableOptions, Shape } from "./hittable";
 import { Vec3 } from "./vec3";
 
 interface PolyOptions extends HittableOptions {
-	points: Array<Vec3>
+	points: [Vec3, Vec3, Vec3] //Array<Vec3>
 }
 
 export class Poly extends Hittable {
@@ -16,5 +16,18 @@ export class Poly extends Hittable {
 		this.points = options.points
 		
 		
+	}
+
+	getSurfaceNormal() { // https://www.khronos.org/opengl/wiki/Calculating_a_Surface_Normal
+		let U:Vec3 = this.points[2].subtract(this.points[1])
+		let V:Vec3 = this.points[3].subtract(this.points[1])
+
+		let normalX = U.y*V.z - U.z*V.y
+		let normalY = U.z*V.x - U.x*V.z
+		let normalZ = U.x*V.y - U.y*V.x
+
+		let normal = new Vec3(normalX, normalY, normalZ)
+
+		return normal
 	}
 }
--- src/public/scripts/ray.ts
+++ src/public/scripts/ray.ts
@@ -1,6 +1,7 @@
 import { Color } from "./color"
-import { Hittable } from "./hittable"
+import { Hittable, Shape } from "./hittable"
 import { world } from "./main"
+import { Poly } from "./poly"
 import { Vec3 } from "./vec3"
 import { World } from "./world"
 
@@ -33,20 +34,37 @@ export class Ray {
 		let hittables = this.world.hittables
 
 		for(let hittable of hittables) {
-			let eye_to_centerBall = hittable.pos.clone().subtract(ray.pos)
-			let rayLength = eye_to_centerBall.dotProduct(ray.vel)
-			let rayClosestToBall = ray.pos.clone().plus(ray.vel.clone().setMagnitude(rayLength))
-			let rayDistanceToBall = rayClosestToBall.clone().subtract(hittable.pos).getMagnitude()
-			if(rayDistanceToBall < hittable.size/2 && rayLength > 0) {
-				let dist1 = rayLength-Math.sqrt((hittable.size/2)**2 - rayDistanceToBall**2)
-				let intersectPos = ray.pos.clone().plus(ray.vel.clone().setMagnitude(dist1))
-				let intersect:rayBallIntersect = {
-					rayLength,
-					hittable,
-					intersectPos,
-					ray
+
+			// Ball shape
+			if(hittable.shape == Shape.ball) {
+				let eye_to_centerBall = hittable.pos.clone().subtract(ray.pos)
+				let rayLength = eye_to_centerBall.dotProduct(ray.vel)
+				let rayClosestToBall = ray.pos.clone().plus(ray.vel.clone().setMagnitude(rayLength))
+				let rayDistanceToBall = rayClosestToBall.clone().subtract(hittable.pos).getMagnitude()
+				if(rayDistanceToBall < hittable.size/2 && rayLength > 0) {
+					let dist1 = rayLength-Math.sqrt((hittable.size/2)**2 - rayDistanceToBall**2)
+					let intersectPos = ray.pos.clone().plus(ray.vel.clone().setMagnitude(dist1))
+					let intersect:rayBallIntersect = {
+						rayLength,
+						hittable,
+						intersectPos,
+						ray
+					}
+					intersects.push(intersect)
 				}
-				intersects.push(intersect)
+			}
+
+			// Poly shape
+			if(hittable.shape == Shape.poly) {
+				let poly = hittable as Poly
+
+				// Check at what point the ray intersects the poly as a plane
+				let planeNormal = poly.getSurfaceNormal()
+				let diff:Vec3 = ray.pos.subtract(poly.points[0])
+        let prod1:number = diff.dotProduct(planeNormal)
+        let prod2:number = ray.vel.dotProduct(planeNormal)
+        let prod3:number = prod1 / prod2
+        let hitPoisition = ray.pos.subtract(ray.vel.multiply(new Vec3(prod3, prod3, prod3)))
 			}
 		}
 		let intersectsOrdered = intersects.sort((a:rayBallIntersect, b:rayBallIntersect) => a.rayLength - b.rayLength)
